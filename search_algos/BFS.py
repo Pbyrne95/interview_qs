e8289d75516b28d9ce4f79d2794f891f87941c97
Philip Byrne <byrnepj@cardiff.ac.uk>
2021-04-20 10:18:35 +0100

    adding BREADTH FIRST SEARCH and DEPTH FIRST SEARCH

---
+++ search_algos/BFS.py
@@ -0,0 +1,19 @@
+def bfs(graph, start_vertex, target_value):
+  path = [start_vertex]
+  vertex_and_path = [start_vertex, path]
+  bfs_queue = [vertex_and_path]
+  visited = set()
+  
+  while bfs_queue:
+    current_vertex, path = bfs_queue.pop(0)
+    visited.add(current_vertex)
+    
+    for neighbor in graph[current_vertex]:
+      # Finish the function here:
+      if neighbor not in visited:
+        if neighbor in [target_value]:
+          path.append(neighbor)
+          return path
+        else:
+          temp = [neighbor, path+[neighbor]]
+          bfs_queue.append(temp)
